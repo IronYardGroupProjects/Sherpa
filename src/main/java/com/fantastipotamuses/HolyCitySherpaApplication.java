035fb3e72de0b7962f99fa63136aabb1daaa0129
Alex <amhughes@g.cofc.edu>
2016-04-05 16:29:02 -0400

    Initial mothership import

---
+++ src/main/java/com/fantastipotamuses/HolyCitySherpaApplication.java
@@ -0,0 +1,12 @@
+package com.fantastipotamuses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolyCitySherpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolyCitySherpaApplication.class, args);
+	}
+}
