dc99fbaacfb07c7f5a5a3cee1a88a274c3d05237
Alex <amhughes@g.cofc.edu>
2016-04-06 16:16:20 -0400

    Still working on db relations

--- src/main/java/com/fantastipotami/entities/TourLocationJoin.java
+++ src/main/java/com/fantastipotami/entities/TourLocationJoin.java
@@ -9,18 +9,23 @@ import java.util.UUID;
  * Created by alexanderhughes on 4/5/16.
  */
 @Entity
-@Table(name = "locations")
-public class Location {
+@Table(name = "tour_loc_joins")
+public class TourLocationJoin {
     @Id
     @GeneratedValue(generator = "uuid2")
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
     @Column(columnDefinition = "BINARY(16)")
     private UUID uuid;
-    String name;
-    @OneToOne
-    Detail detail;
-    Boolean visited;
-    @ManyToOne
-    Category category;
 
+    private Boolean visited = false;
+
+    @ManyToOne
+    private Detail detail;
+    @ManyToOne
+    private Tour tour;
+
+    public TourLocationJoin(Detail detail, Tour tour) {
+        this.detail = detail;
+        this.tour = tour;
+    }
 }
