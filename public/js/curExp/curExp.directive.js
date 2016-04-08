65fffdbd3d4214a5753850519c609e1cfcd7bf4e
EricRClark <eric.erandy@gmail.com>
2016-04-08 14:46:25 -0400

    curExp service and directive updated

--- public/js/curExp/curExp.directive.js
+++ public/js/curExp/curExp.directive.js
@@ -4,12 +4,17 @@ angular
   .directive('experienceDirective', function (){
 
     return{
-      /*restrict: Not sure if this will be both or just Element or Attribute */
+      restrict: 'EA',
       templateUrl: 'curExp/templates/experience.directive.html',
       /*come back to this transclusion? link? click event changes? */
     scope: {
-      tour: '',
-      category: '',
+      tour: '@',
+      tourID: '@'
     },
+    link: function (scope, element, attributes) {
+      element.on('click', function (event){
+        
+      })
+    }
     }
   })
--- public/js/curExp/curExp.service.js
+++ public/js/curExp/curExp.service.js
@@ -2,9 +2,20 @@ var angular = require('angular');
 
 angular
   .module('curExp')
-  .factory('CurExpService', function($http, ))
+  .factory('CurExpService', function($http) {
 
-  .service('curExpService', function(){
-    return{
+    var permTour = '/perm-tour';
+
+    var getTours = function () {
+      return $http.get(permTour)
     }
-  })
+    var getTour = function (id) {
+      return $http.get(permTour + '/' + id)
+    }
+
+  return {
+      getTour: getTour,
+      getTours: getTours
+  }
+
+});
