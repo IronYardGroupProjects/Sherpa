3613a2eeb5acd7941e8a79ec2db6d196e8850dbb
Alex <amhughes@g.cofc.edu>
2016-04-12 09:03:51 -0400

    have one prebuilt tour

--- src/main/java/com/fantastipotami/controllers/SherpaController.java
+++ src/main/java/com/fantastipotami/controllers/SherpaController.java
@@ -51,6 +51,92 @@ public class SherpaController {
         populateCategoriesTable("categories.tsv");
         populateLocationsTable("locations.tsv");
         populatePermToursTable("permTours.tsv");
+        populateTourTable(new HttpSession() {
+            @Override
+            public long getCreationTime() {
+                return 0;
+            }
+
+            @Override
+            public String getId() {
+                return null;
+            }
+
+            @Override
+            public long getLastAccessedTime() {
+                return 0;
+            }
+
+            @Override
+            public ServletContext getServletContext() {
+                return null;
+            }
+
+            @Override
+            public void setMaxInactiveInterval(int interval) {
+
+            }
+
+            @Override
+            public int getMaxInactiveInterval() {
+                return 0;
+            }
+
+            @Override
+            public HttpSessionContext getSessionContext() {
+                return null;
+            }
+
+            @Override
+            public Object getAttribute(String name) {
+                return null;
+            }
+
+            @Override
+            public Object getValue(String name) {
+                return null;
+            }
+
+            @Override
+            public Enumeration<String> getAttributeNames() {
+                return null;
+            }
+
+            @Override
+            public String[] getValueNames() {
+                return new String[0];
+            }
+
+            @Override
+            public void setAttribute(String name, Object value) {
+
+            }
+
+            @Override
+            public void putValue(String name, Object value) {
+
+            }
+
+            @Override
+            public void removeAttribute(String name) {
+
+            }
+
+            @Override
+            public void removeValue(String name) {
+
+            }
+
+            @Override
+            public void invalidate() {
+
+            }
+
+            @Override
+            public boolean isNew() {
+                return false;
+            }
+        }, 2);
     }
 
     @PreDestroy
@@ -200,5 +286,8 @@ public class SherpaController {
             PermTourLocationJoin tlj = new PermTourLocationJoin(locRepo.findOne(Integer.valueOf(columns[1])), pTourRepo.findOne(Integer.valueOf(columns[0])));
             pTourLocRepo.save(tlj);
         }
+    }
+    public void populateTourTable(HttpSession session, int id) {
+        joinTour(session, id);
     }
 }
