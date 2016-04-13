eecb358f90332880484bd49bb1a5e72d5d1d68cd
EricRClark <eric.erandy@gmail.com>
2016-04-13 14:44:59 -0400

    updated tour modals in curExp folder

--- public/js/curExp/curExp.controller.js
+++ public/js/curExp/curExp.controller.js
@@ -1,4 +1,6 @@
 var angular = require('angular');
+var $ = jquery = require('jquery');
+var bootstrap = require('bootstrap');
 angular
     .module('curExp')
     .controller('curExpController', function($rootScope, $scope, CurExpService) {
@@ -12,6 +14,12 @@ angular
                 // $window.localStorage.setItem('tourId', response);
                 vm.tours = data.data;
                 // window.glob=data.data;
+
             });
 
+        vm.sendTour = function (id) {
+        CurExpService.sendSelectedTour(id)  
+        }
     });
+
+//Updated tourID confirmation to send to Alex??
