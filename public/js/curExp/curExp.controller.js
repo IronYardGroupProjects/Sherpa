adf220213f836cd088709dc7f9a074bad1b508c2
bhare1987 <brandonlhare@gmail.com>
2016-04-17 19:31:09 -0400

    got background images for curated view and started with tests

--- public/js/curExp/curExp.controller.js
+++ public/js/curExp/curExp.controller.js
@@ -5,15 +5,9 @@ angular
     .module('curExp')
     .controller('curExpController', function($rootScope, $state, $scope, CurExpService) {
         var vm = this;
-        // vm.tours = CurExpService.showTours();
-        // vm.tour = CurExpService.showTour();
-
         CurExpService.getTours()
             .then(function(data) {
-                // $window.localStorage.setItem('tourId', response);
                 vm.tours = data.data;
-                // window.glob=data.data;
-
             });
 
         vm.sendTour = function (id) {
